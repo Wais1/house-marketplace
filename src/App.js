16a85e05d09cf312b4e2693fe4bbee23d819fa20
Wais1 <waisibrahim77@gmail.com>
2022-01-04 19:40:41 +0700

    added alerts and database signup/signin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers';
@@ -15,12 +17,14 @@ function App() {
         <Route path='/' element={<Explore />}></Route>
         <Route path='/offers' element={<Offers />}></Route>
         <Route path='/profile' element={<Profile/>}></Route>
-        <Route path='/sign-in' element={<Profile />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
         <Route path='/sign-up' element={<SignUp />}></Route>
         <Route path='/forgot-password' element={<ForgotPassword />}></Route>
       </Routes>
       <Navbar></Navbar>
     </Router>
+
+    <ToastContainer />
     </>
   );
 }
